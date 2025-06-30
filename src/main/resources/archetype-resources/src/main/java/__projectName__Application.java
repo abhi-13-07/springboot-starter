7f42520ecd347acd9cbb8f0904a520c98500953d
abhi-13-07 <abhishekp.0702@gmail.com>
2025-07-01 00:56:27 +0530

    add project name property and populate main file with

--- src/main/resources/archetype-resources/src/main/java/__projectName__Application.java
+++ src/main/resources/archetype-resources/src/main/java/__projectName__Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class ${projectName}Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(${projectName}Application.class, args);
         System.out.println("Hello World!");
     }
 
